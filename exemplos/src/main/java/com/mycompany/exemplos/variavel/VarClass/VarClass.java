34012a414cbc0e3cd622ffd09adee41d5b2aa60e
HIGOR PIRES <hgrcrls@gmail.com>
2024-12-22 15:38:40 -0300

    add

---
+++ exemplos/src/main/java/com/mycompany/exemplos/variavel/VarClass/VarClass.java
@@ -0,0 +1,9 @@
+package VarClass;
+
+public class VarClass {
+    static int i = 0; //variável de classe não precisa de um objeto para ser manipulada 
+   public static void main(String[] args){
+ System.out.println(VarClass.i);
+}
+} 
+
