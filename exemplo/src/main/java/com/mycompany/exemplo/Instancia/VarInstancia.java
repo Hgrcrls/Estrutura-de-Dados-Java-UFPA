44499c63621ae0e48fbac534295851c0e5a82fcd
HIGOR PIRES <hgrcrls@gmail.com>
2024-12-21 22:49:04 -0300

    Update VarInstancia.java

--- exemplo/src/main/java/com/mycompany/exemplo/Instancia/VarInstancia.java
+++ exemplo/src/main/java/com/mycompany/exemplo/Instancia/VarInstancia.java
@@ -1,15 +1,19 @@
 package com.mycompany.exemplo.Instancia;
 
 class VarInstancia {
-    void imprimeVar(){
-        int i = 1; //variavel local
-        String s = "Bom dia"; //variavel local
-        System.out.println(s);
-        System.out.println(i);
+    int i = 0; //variavel de instancia
+ void imprimeVar1(){
+     System.out.println(i);
+     i += 2;       
 }
-
+ void imprimeVar2(){
+     System.out.println(i);
+     i+= 3;
+ }
     public static void main(String[] args) {
         VarInstancia v = new VarInstancia();
-        v.imprimeVar();
+        v.imprimeVar1();
+        v.imprimeVar2();
+        System.out.println(v.i); //variavel i sendo manipulada por um objeto da sua classe      
     }
 }
